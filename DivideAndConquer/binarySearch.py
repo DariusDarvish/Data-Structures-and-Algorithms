4aefd06892dee151b0f118c42b96c05282c76328
DariusDarvish <dariusdarvish@Dariuss-MacBook-Pro.local>
2026-09-05 16:56:15 -0400

    Adding DivideAndConquer

---
+++ DivideAndConquer/binarySearch.py
@@ -0,0 +1,29 @@
+
+def binarySearch(target,shelf,left=None,right=None):
+    if left is None:
+        left=0
+        
+    if right is None:
+        right=len(shelf)-1
+    # Base case
+    if left>right:
+        return False
+
+    midpoint=(left+right)//2
+    
+    # Base case
+    if shelf[midpoint]==target:
+        return True
+
+    elif shelf[midpoint]>target:
+        return binarySearch(target,shelf,left,right-1)
+
+    elif shelf[midpoint]<target:
+        return binarySearch(target,shelf,left+1,right)
+
+
+print(binarySearch(3,[1,2,3,4,5,6,7,8,9]))
+    
+
+    
+    
