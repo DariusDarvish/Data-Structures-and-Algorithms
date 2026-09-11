7228f6c964fb16e4027316a26d513f64623e168d
DariusDarvish <dariusdarvish@Dariuss-MacBook-Pro.local>
2026-09-11 15:31:16 -0400

    Permutations practice

--- Permutations/dfsSeatingChart.py
+++ Permutations/dfsSeatingChart.py
@@ -1,16 +1,16 @@
 visited=[]
-def seatingCharts(seating_chart,total_length,current_chart):
-    if len(current_chart)==total_length:
+def seatingCharts(seating_chart,current_chart):
+    if len(current_chart)==len(seating_chart):
         visited.append(current_chart.copy())
         return
     
     for x in seating_chart:
         if x not in current_chart:
             current_chart.append(x)
-            seatingCharts(seating_chart,total_length,current_chart)
+            seatingCharts(seating_chart,current_chart)
             #This step is the key to set up the search by removing the charatcer we are allowing for us to create the recursion branches
             current_chart.pop()
 
     return visited
 
-print(seatingCharts(['Alice','Ben','Claude'],3,current_string=[]))
+print(seatingCharts(['Alice','Ben','Claude'],current_chart=[]))
